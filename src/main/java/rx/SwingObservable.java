12223cd670e8b70f1b429c87faa7ead7e5cdefc7
jmhofer <jmhofer.github@johoop.de>
2013-05-02 19:20:06 +0200

    Moved Swing observables to new subfolder `rxjava-contrib` after merge.

---
+++ src/main/java/rx/SwingObservable.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright 2013 Netflix, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package rx;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractButton;
+
+import rx.swing.sources.AbstractButtonSource;
+
+/**
+ * Allows creating observables from various sources specific to Swing. 
+ */
+public enum SwingObservable { ; // no instances
+
+    /**
+     * Creates an observable corresponding to a Swing button action.
+     * 
+     * @param button 
+     *            The button to register the observable for.
+     * @return Observable of action events.
+     */
+    public static Observable<ActionEvent> fromButtonAction(AbstractButton button) {
+        return AbstractButtonSource.fromActionOf(button);
+    }
+}
