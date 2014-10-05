30b01f1a053a3093db7b2cd3b3fea75c799ed6cc
bruno marinho <bdmstyle2@gmail.com>
2014-10-05 20:13:47 -0300

    fix unit test

--- src/test/java/rx/swing/sources/KeyEventSourceTest.java
+++ src/test/java/rx/swing/sources/KeyEventSourceTest.java
@@ -95,7 +95,7 @@ public class KeyEventSourceTest {
                         .subscribe(action, error, complete);
 
                 InOrder inOrder = inOrder(action);
-                inOrder.verify(action, times(1)).call(
+                inOrder.verify(action, never()).call(
                         Collections.<Integer> emptySet());
                 verify(error, never()).call(Matchers.<Throwable> any());
                 verify(complete, never()).call();
